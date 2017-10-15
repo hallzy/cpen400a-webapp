75a69639e56bd866b8ca2d29304315bca955a9f8
Steven Hall <Hallzy.18@gmail.com>
2017-10-14 18:47:28 -0700

    Resolves #16 and contributes a lot to #15

--- js/main.js
+++ js/main.js
@@ -89,6 +89,35 @@ window.onload = function() {
   timer.set();
 
   generateProducts();
+
+
+
+
+
+  var modal = document.getElementById('cartModal');
+  var btn = document.getElementById("show_cart");
+  var span = document.getElementsByClassName("close")[0];
+
+  btn.onclick = function() {
+    modal.style.display = "block";
+  }
+
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+
+  document.onkeydown = function(key) {
+    // 27 is the key for the escape button
+    if (key.keyCode == 27) {
+      modal.style.display = "none";
+    }
+  }
 }
 
 // Prototype function for Product objects that computes the price of quantity
